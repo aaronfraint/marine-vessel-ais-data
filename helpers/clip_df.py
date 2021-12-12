49f91e69b63c6943cb142e2026ebb7b7f27f3584
aaronfraint <aaronfraint@icloud.com>
2021-12-12 10:57:28 -0500

    update column names

--- helpers/clip_df.py
+++ helpers/clip_df.py
@@ -5,12 +5,12 @@ from shapely.geometry import Polygon
 
 def clip_df_to_region(df: pd.DataFrame, clipping_shape: Polygon) -> gpd.GeoDataFrame:
     """
-    Turn a non-spatial dataframe with LON/LAT columns into a point geodataframe,
+    Turn a non-spatial dataframe with lon/lat columns into a point geodataframe,
     and then clip it to the provided `clipping_shape`.
 
     Return as a filtered geodataframe
     """
     data_gdf = gpd.GeoDataFrame(
-        df, geometry=gpd.points_from_xy(df["LON"], df["LAT"], crs="EPSG:4326")
+        df, geometry=gpd.points_from_xy(df["lon"], df["lat"], crs="EPSG:4326")
     )
     return data_gdf[data_gdf.intersects(clipping_shape)]
