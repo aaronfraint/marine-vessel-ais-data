9e1b24bd6a3f3dbbc4475baa3584e53e301b6f0a
aaronfraint <aaronfraint@icloud.com>
2021-12-12 10:46:56 -0500

    add environment

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="marine-vessel-ais-data",
+    packages=find_packages(),
+    version="0.0.1",
+    description="Scripts to facilitate the download, extraction, and usage of data from https://marinecadastre.gov/ais/",
+    author="Aaron Fraint, AICP",
+    license="GPL-3.0",
+)
