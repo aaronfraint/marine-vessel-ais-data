72bd4de4390101695bffa3c4ce6ab97591d0e710
aaronfraint <aaronfraint@icloud.com>
2021-12-13 12:21:02 -0500

    break chunks out to take advantage of timing decorator

--- scripts/import_csvs.py
+++ scripts/import_csvs.py
@@ -1,44 +1,53 @@
 import sys
 import pandas as pd
+import geopandas as gpd
 from pathlib import Path
+from sqlalchemy import sql
+from tqdm import tqdm
 from geoalchemy2 import Geometry, WKTElement
 
-from helpers import get_dvrpc_polygon, clip_df_to_region, extract_zipfile, engine, DOWNLOAD_FOLDER
+from helpers import (
+    get_dvrpc_polygon,
+    clip_df_to_region,
+    extract_zipfile,
+    engine,
+    DOWNLOAD_FOLDER,
+    days_in_table,
+    print_message_and_timer,
+)
 
 
-def import_single_zip_file(zipped_filepath: Path) -> None:
-    """
-    All business logic needed to transform a zipped data file into
-    rows in a postgres/postgis database table. This will use a tablename
-    that uses the year with a prefix of 'ais_'. e.g. 'ais_2019`
+@print_message_and_timer("Reading raw CSV")
+def read_csv(unzipped_filepath: Path):
+    df = pd.read_csv(
+        unzipped_filepath,
+        usecols=[
+            "BaseDateTime",
+            "LAT",
+            "LON",
+            "SOG",
+            "VesselName",
+            "VesselType",
+            "Status",
+            "Length",
+            "Width",
+            "Draft",
+            "Cargo",
+        ],
+    )
 
-    - Unzip file to CSV
-    - Clip spatialized CSV to DVRPC region
-    - Transform geometries from shapely to WKT
-    - Import into database
-    - Delete CSV (but not the zip file)
-    """
-
-    # SHOULD CHECK FIRST TO SEE IF ANY ROWS EXIST WITH THIS DATE IN THE DB!
-
-    # Make a name for the table in the database: use a single table for each year
-    sql_tablename = "ais_" + zipped_filepath.stem.split("_")[1]
-
-    # unzip single file
-    unzipped_filepath = extract_zipfile(zipped_filepath)
-
-    # read csv as tabular dataframe
-    print("\t -> Reading full CSV")
-    df = pd.read_csv(unzipped_filepath)
     df.columns = [x.lower() for x in df.columns]
 
-    # spatialize dataframe and clip it
-    print("\t -> Clipping spatialized CSV to region")
-    dvrpc_polygon = get_dvrpc_polygon()
-    clipped_gdf = clip_df_to_region(df, dvrpc_polygon)
+    return df
 
-    # convert shapely geometry to WKT
-    print("\t -> Writing to database")
+
+@print_message_and_timer("Deleting CSV")
+def delete_csv(filepath: Path) -> None:
+    filepath.unlink()
+
+
+@print_message_and_timer("Importing to db")
+def import_to_db(clipped_gdf: gpd.GeoDataFrame, sql_tablename: str):
     clipped_gdf["geom"] = clipped_gdf["geometry"].apply(lambda x: WKTElement(x.wkt, srid=4326))
     clipped_gdf.drop(
         labels="geometry",
@@ -56,19 +65,58 @@ def import_single_zip_file(zipped_filepath: Path) -> None:
         if_exists="append",
     )
 
-    # Delete CSV
-    print("\t -> Deleting CSV")
-    unzipped_filepath.unlink()
+
+def import_single_zip_file(zipped_filepath: Path, sql_tablename: str) -> None:
+    """
+    All business logic needed to transform a zipped data file into
+    rows in a postgres/postgis database table. This will use a tablename
+    that uses the year with a prefix of 'ais_'. e.g. 'ais_2019`
+
+    - Unzip file to CSV
+    - Clip spatialized CSV to DVRPC region
+    - Transform geometries from shapely to WKT
+    - Import into database
+    - Delete CSV (but not the zip file)
+    """
+
+    # unzip single file
+    unzipped_filepath = extract_zipfile(zipped_filepath)
+
+    # read csv as tabular dataframe
+    df = read_csv(unzipped_filepath)
+
+    # spatialize dataframe and clip it
+    dvrpc_polygon = get_dvrpc_polygon()
+    clipped_gdf = clip_df_to_region(df, dvrpc_polygon)
+
+    # import to database
+    import_to_db(clipped_gdf, sql_tablename)
+
+    # delete unzipped CSV
+    delete_csv(unzipped_filepath)
 
 
 def import_a_year_of_files(year: int) -> None:
     print(f"IMPORTING ALL ZIP FILES FOR {year}")
 
-    zipfiles_to_process = sorted(list(DOWNLOAD_FOLDER.rglob(f"{year}/*.zip")))
+    sql_tablename = f"raw_data.ais_{year}"
 
-    for zipfilepath in zipfiles_to_process:
+    days_that_were_previously_imported = days_in_table(sql_tablename)
+
+    all_zipfiles = sorted(list(DOWNLOAD_FOLDER.rglob(f"{year}/*.zip")))
+
+    zipfiles_to_process = [
+        x
+        for x in all_zipfiles
+        if x.stem.replace("_", "-").replace("AIS-", "") not in days_that_were_previously_imported
+    ]
+
+    for zipfilepath in tqdm(zipfiles_to_process):
         print(zipfilepath)
-        import_single_zip_file(zipfilepath)
+
+        sql_tablename = "ais_" + zipfilepath.stem.split("_")[1]
+
+        import_single_zip_file(zipfilepath, sql_tablename)
 
 
 if __name__ == "__main__":
