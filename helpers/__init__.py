fcf440c96a1cb7f2f2f8d38d4d625983d1f25320
aaronfraint <aaronfraint@icloud.com>
2021-12-13 12:20:50 -0500

    update imports

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -3,3 +3,5 @@ from .get_polygon import get_dvrpc_polygon
 from .unzip_file import extract_zipfile
 from .database import engine
 from .env_vars import DOWNLOAD_FOLDER
+from .is_this_table_in_the_db import days_in_table
+from .timer import print_message_and_timer
